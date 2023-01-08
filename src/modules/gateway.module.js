c6b7274414c5d2a3fa2ad974982f1b465647acc6
acm-97 <alejandro97.acm@gmail.com>
2023-01-08 04:00:12 -0500

    feat: add modules

---
+++ src/modules/gateway.module.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const gatewaySchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A name for the gateway is required']
+  },
+  ipv4_address: {
+    type: String,
+    unique: true,
+    required: [true, 'A ipv4 address for the gateway is required']
+  }
+}, {
+  timestamps: true
+})
+
+const Gateway = mongoose.model('gateway', gatewaySchema)
+module.exports = Gateway
---
+++ src/modules/peripheral.module.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const peripheralSchema = new Schema({
+  uid: {
+    type: Number,
+    unique: true,
+    required: [true, 'A UID for the peripheral is required']
+  },
+  vendor: {
+    type: String,
+    unique: true,
+    required: [true, 'A vendor for the peripheral is required']
+  },
+  status: {
+    type: String,
+    enum: ['offline', 'online'],
+    default: 'online'
+  },
+  gateway: {
+    type: Schema.ObjectId,
+    ref: 'gateway',
+    required: [true, 'A gateway for the peripheral is required']
+  }
+}, {
+  timestamps: true
+})
+
+const Peripheral = mongoose.model('peripheral', peripheralSchema)
+module.exports = Peripheral
