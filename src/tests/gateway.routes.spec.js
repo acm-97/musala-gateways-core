8766b68a1dbfb26e3dce881026521b1b56126d03
acm-97 <alejandro97.acm@gmail.com>
2023-01-10 22:32:06 -0500

    test: add delete test for gateway

--- src/tests/gateway.routes.spec.js
+++ src/tests/gateway.routes.spec.js
@@ -24,7 +24,7 @@ describe('Gateways routes tests', () => {
     server.close()
   })
 
-  describe('POST /api/gateways', () => {
+  describe('POST /api/gateway', () => {
     it('Response 200', async () => {
       const response = await api.post('/api/gateway').send(paramas)
       expect(response.status).toBe(200)
@@ -53,7 +53,7 @@ describe('Gateways routes tests', () => {
     })
   })
 
-  describe('GET /api/gateways/:id', () => {
+  describe('GET /api/gateway/:id', () => {
     it('Response 200', async () => {
       const gates = await api.get('/api/gateways').send()
       const response = await api.get(`/api/gateway/${gates.body[0]?._id.toString()}`).send()
@@ -75,7 +75,7 @@ describe('Gateways routes tests', () => {
     // })
   })
 
-  describe('PATCH /api/gateways/:id', () => {
+  describe('PATCH /api/gateway/:id', () => {
     let gate = null
     beforeAll(async () => {
       gate = await Gateway.findOne({ name: paramas.name })
@@ -93,6 +93,26 @@ describe('Gateways routes tests', () => {
       expect(response.status).toBe(400)
       expect(response.headers['content-type']).toContain('json')
       expect(response.body).toEqual(`${wrongParamas.ipv4_address} is an invalid IP or is already in use`)
+    })
+  })
+
+  describe('DELETE /api/gateway/:id', () => {
+    let gate = null
+    beforeAll(async () => {
+      gate = await Gateway.findOne({ name: paramas.name })
+    })
+
+    it('Response 200', async () => {
+      const response = await api.delete(`/api/gateway/${gate?._id?.toString()}`).send(paramas)
+      expect(response.status).toBe(200)
+      expect(response.headers['content-type']).toContain('json')
+      expect(response.body).toBeInstanceOf(Object)
+    })
+
+    it('Response 404', async () => {
+      const response = await api.delete('/api/gateway/wrong_id').send()
+      expect(response.status).toBe(400)
+      expect(response.headers['content-type']).toContain('json')
     })
   })
 })
